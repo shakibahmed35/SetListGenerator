54b1e0d3dcc646a8b08af277e9e3d31df081c253
shaydoge <shayaanahmed7285@gmail.com>
2025-01-03 20:12:09 -0700

    made scraper function

--- scraper.py
+++ scraper.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = input("Please Give URL of the Setlist from Setlist.fm: ")  
-response = requests.get(url)
-html_content = response.text
+def get_songs(url):
+
+   
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
 
 
-soup = BeautifulSoup(html_content, 'html.parser')
+    span_elements = soup.find_all(class_="songLabel")
 
 
-span_elements = soup.find_all(class_="songLabel")
+    for span in span_elements:
+        print(span.text)  
 
-
-for span in span_elements:
-    print(span.text)  
+if __name__ == "__main__":
+    url = input("Please Give URL of the Setlist from Setlist.fm: ")
+    get_songs(url)
+    artist_name = input("Please Give Artists Name: ")
