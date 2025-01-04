a1739dba4c8090a6fe51da09782f0eb8378ca9f1
shaydoge <shayaanahmed7285@gmail.com>
2025-01-03 20:56:45 -0700

    adding return bc im dumb

--- scraper.py
+++ scraper.py
@@ -13,10 +13,11 @@ def get_songs(url):
     span_elements = soup.find_all(class_="songLabel")
 
 
-    for span in span_elements:
-        print(span.text)  
+    songs = [span.text for span in span_elements] 
+    artist_name = input("Please Give Artists Name: ")
+    return songs, artist_name
 
 if __name__ == "__main__":
     url = input("Please Give URL of the Setlist from Setlist.fm: ")
     get_songs(url)
-    artist_name = input("Please Give Artists Name: ")
+    
