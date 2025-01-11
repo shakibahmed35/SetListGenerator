bdc0c811d3f7f5557928efc284f963f0a1e449f1
Shakib Ahmed <shakibahmed@Shakibs-MacBook-Air.local>
2025-01-10 19:21:31 -0700

    Final playlist creation

--- playlistGenerator.py
+++ playlistGenerator.py
@@ -2,6 +2,7 @@ import spotipy
 import os
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
+from scraper import get_songs
 
 load_dotenv('.env')
 
@@ -18,5 +19,28 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
 user_profile = sp.current_user()
 user_id = user_profile['id']  # This gives the user's Spotify ID
 
+url = input("Please Give URL of the Setlist from Setlist.fm: ")
 
-sp.user_playlist_create(user_id, "test", public=True, collaborative=False, description='Testing the spotipy api')
+[songs, name] = get_songs(url)
+uris = []
+
+for song in songs:
+    try:
+        query = f"track:{song} artist:{name}"
+        result = sp.search(q=query, type="track", limit=1)
+
+        if result['tracks']['items']:
+            uris.append(result['tracks']['items'][0]['uri'])
+        else:
+            raise IndexError
+    except IndexError:
+        print(f"Error: Could not find {song} by {name}")
+
+
+new_playlist = sp.user_playlist_create(user_id, f'{name} setlist', public=True, collaborative=False, description=f'Creating Setlist for {name}')
+playlist_id = new_playlist['id']
+
+if uris:
+    sp.playlist_add_items(playlist_id=playlist_id, items=uris)
+else:
+    print('No songs to add')
